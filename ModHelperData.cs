4d5e5afe7986d074da5c0198d6720ccef15f90ac
HyGamer2010 <115854591+HyGamer2010@users.noreply.github.com>
2023-01-02 20:53:52 -0800

    Update

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -7,6 +7,6 @@ public static class ModHelperData
 
     public const string Description = "The Space Monkey is here to save the day! Adds a tower themed around space";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "HyGamer2010"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "SpaceMonkey"; // TODO add your repo name here, also in the download url in README.md
 }
